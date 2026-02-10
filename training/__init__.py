62088211f05ccee13405466bf616ca3c56e8e54c
everdzekov <108196154+everdzekov@users.noreply.github.com>
2026-02-10 11:20:25 +0100

    Delete training directory

--- training/__init__.py
+++
@@ -1,9 +0,0 @@
-from .trainer import EnhancedPhysGazeTrainer
-from .losses import compute_physgaze_losses
-from .dynamic_epoch import DynamicEpochManager
-
-__all__ = [
-    'EnhancedPhysGazeTrainer',
-    'compute_physgaze_losses',
-    'DynamicEpochManager'
-]
